0dbd4a1892c48c3d2206fcd5ad61b21d2526c1dc
pivovarnya <gulnaz.pakhrieyva.06@mail.ru>
2026-06-15 22:56:10 +0500

    fix task3

--- server.js
+++ server.js
@@ -2,31 +2,20 @@ const http = require("http");
 const url = require("url");
 
 function gcd(a, b) {
-    while (b != 0) {
-        let c = b;
+    while (b) {
+        let t = b;
         b = a % b;
-        a = c;
+        a = t;
     }
     return a;
 }
 
-function lcm(a, b ) {
-    return (a * b / gcd(a, b));
+function lcm(a, b) {
+    return (a * b) / gcd(a, b);
 }
 
-const server = http.createServer((req, res) => {
-    let params = url.parse(req.url, true).query;
+http.createServer((req, res) => {
+    const { x, y } = url.parse(req.url, true).query;
 
-    let x = Number(params.x);
-    let y = Number(params.y);
-
-    if (isNaN(x) || isNaN(y) || x <= 0 || y <= 0) {
-        res.end("NaN");
-    }
-    else {
-        res.end(String(lcm(x, y)));
-    }
-
-});
-
-server.listen(process.env.PORT || 3000);
+    res.end(String(lcm(Number(x), Number(y))));
+}).listen(process.env.PORT || 3000);
